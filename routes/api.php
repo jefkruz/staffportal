186068586aa41fa909eb7f3a89addd734c01396c
jefkruz <josephugbeva@gmail.com>
2024-06-28 13:20:20 +0100

    helo

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('verify', [ApiController::class, 'verify']);
 
+Route::post('staff/auth/callback', [KcController::class, 'authorizeLogin'])->name('authLogin');
